2737bfa308b1f078352e4c3015792269717f2e37
sergey <sergey@cimon.io>
2019-06-22 21:38:19 +0300

    Fix port

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :presence_chat, PresenceChatWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "#{System.get_env("APP_NAME")}.gigalixirapp.com", port: 80],
+  url: [host: "#{System.get_env("APP_NAME")}.gigalixirapp.com", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
