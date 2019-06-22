9d3b9c07b2bb5243e4dd1f72f1e651e09685cfa6
sergey <sergey@cimon.io>
2019-06-22 22:06:25 +0300

    revert ssl

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule PresenceChat.MixProject do
   def application do
     [
       mod: {PresenceChat.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
